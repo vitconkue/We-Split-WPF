6ad1bbc184d1be19a8be9fd01b6d358a91f6e2ab
Hoa-Vo <dev.h1620@gmail.com>
2020-12-11 03:22:40 +0700

    detail UI and update home UI

--- View/DetailPage.xaml.cs
+++ View/DetailPage.xaml.cs
@@ -24,5 +24,10 @@ namespace We_Split_WPF.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new View.HomePage());
+        }
     }
 }
