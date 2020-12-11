adbee950a8ddb07246dbbde080787aa2bba78d3d
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-12-11 20:09:33 +0700

    init model class

---
+++ Model/TripModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_Split_WPF.Model
+{
+    class TripModel
+    {
+    }
+}
