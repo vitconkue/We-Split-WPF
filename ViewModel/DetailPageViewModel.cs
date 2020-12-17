b0d048df2159b9ef2854cc9e34920f562891d382
Hoa-Vo <dev.h1620@gmail.com>
2020-12-17 19:49:26 +0700

    get image link

--- ViewModel/DetailPageViewModel.cs
+++ ViewModel/DetailPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,22 +10,59 @@ using We_Split_WPF.Command;
 using We_Split_WPF.Model;
 namespace We_Split_WPF.ViewModel
 {
-    public class DetailPageViewModel: BaseViewModel
+    public class DetailPageViewModel : BaseViewModel
     {
         public TripModel Trip { get; set; }
         private MainViewModel viewModel;
         public ICommand UpdateTrip { get; set; }
-        
+        private Uri _placeImageDisplay;
+        private string _currentPlaceImage = "1";
+        private string _totalPlaceImage;
+        public Uri PlaceImageDisplay
+        {
+            get
+            {
+                return _placeImageDisplay;
+            }
+            set
+            {
+                _placeImageDisplay = value;
+                OnPropertyChanged(nameof(PlaceImageDisplay));
+            }
+        }
+        public string CurrentPlaceImage
+        {
+            get
+            {
+                return _currentPlaceImage;
+            }
+            set
+            {
+                _currentPlaceImage = value;
+                OnPropertyChanged(nameof(CurrentPlaceImage));
+            }
+            }
+        public string TotalPlaceImage
+        {
+            get
+            {
+                return _totalPlaceImage;
+            }
+            set
+            { 
+                _totalPlaceImage = value;
+                OnPropertyChanged(nameof(TotalPlaceImage));
+            }
+        }
+        public List<Uri> PlaceImages { get; set; }
         public DetailPageViewModel(int ID,MainViewModel param)
         {
             Trip = DatabaseAccess.LoadSingleTrip(ID);
             Trip.Name = Trip.Name.ToUpper();
             this.viewModel = param;
             UpdateTrip = new UpdateTripCommand(viewModel,ID);
-            string file = AppDomain.CurrentDomain.BaseDirectory;
-            string path = $"{file}Data\\Images\\TripsImage\\{ID}\\Location\\";
-            DirectoryInfo Folder = new DirectoryInfo(path);
-            var Images = Folder.GetFiles();
+            PlaceImages = Trip.PlaceImages;
+            PlaceImageDisplay = PlaceImages[0];
    
         }
     }
